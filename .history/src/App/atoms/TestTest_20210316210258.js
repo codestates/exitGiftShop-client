4ec9e4feb30b963097710a4f78ac52769eeb0de4
= <=>
2021-03-23 10:29:50 +0900

    Feature Collections #28

--- .history/src/App/atoms/TestTest_20210316210258.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function TestTest() {
-  return <div></div>;
-}
-
-export default TestTest;
--- .history/src/Routes/MyBid_20210316214644.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MyBid() {
-  return <div>MyBid</div>;
-}
-
-export default MyBid;
