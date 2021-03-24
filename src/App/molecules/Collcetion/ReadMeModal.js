86cdf9642484f443b1f4ed85b526960d5c579f84
= <=>
2021-03-25 04:44:11 +0900

    Collection & Support Page CSS Update

--- src/App/molecules/Collcetion/ReadMeModal.js
+++ src/App/molecules/Collcetion/ReadMeModal.js
@@ -4,49 +4,83 @@ import { useSelector, useDispatch } from "react-redux";
 
 const ModalOverlay = styled.div`
   box-sizing: border-box;
+  position: fixed;
   display: ${(props) => (props.visible ? "block" : "none")};
-  position: relative;
-  top: -1000px;
-  left: 0px;
-  bottom: 0;
-  right: 0px;
   z-index: 1;
 `;
 
 const ModalWrapper = styled.div`
-  box-sizing: content-box;
-  position: absolute;
+  box-sizing: border-box;
   background-color: white;
-  margin: 20px;
-  padding: 20px;
-  top: 1889px;
-  left: 80px;
-  width: 1400px;
+  position: fixed;
+  border-radius: 10px;
   display: ${(props) => (props.visible ? "block" : "none")};
   z-index: 2;
   transform: scale(0.95);
 `;
 
 const ModalInner = styled.div`
-  box-sizing: border-box;
-  position: -webkit-sticky;
-  position: sticky;
-  top: 200px;
-  box-shadow: 10px 10px
-  background-color: #fff;
+  box-sizing: content-box;
+  display: flex;
+  flex-direction: row;
+  background-color: #cfcfcf;
+  box-shadow: 15px 10px 6px #8a8a8a;
+  border: 3px solid black;
+  position: fixed;
+  float: center;
   border-radius: 10px;
-  transform: translateY(-50%);
   padding: 20px;
+  height: 50em;
+  width: 85em;
   color: black;
   outline: none;
   display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  align-items: center;
+  text-align: left;
+  animation: fadeIn 1s;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  // animation: fadeOut 2s;
+  // @keyframes fadeOut {
+  //   from {
+  //     opacity: 1;
+  //   }
+  //   to {
+  //     opacity: 0;
+  //   }
+  // }
+  span {
+    padding: 2em;
+    padding-top: 0;
+  }
+  img {
+    box-sizing: content-box;
+    padding: 0.6em;
+    width: 41em;
+    object-fit: cover;
+    overflow: hidden;
+  }
+  h1 {
+    margin-bottom: 0.5em;
+    font-family: "Roboto Mono", monospace;
+    font-size: 40px;
+    color: black;
+    font-weight: 900;
+  }
+  div {
+    font-family: "Roboto", sans-serif;
+    font-size: 18px;
+  }
 `;
 
 function ReadMeModal({ className, onClose, maskClosable, visible }) {
-  // const file_path = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/file/`;
+  const file_path = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/file/`;
   const dispatch = useDispatch();
   useEffect(() => {}, [dispatch]);
 
@@ -74,8 +108,14 @@ function ReadMeModal({ className, onClose, maskClosable, visible }) {
         visible={visible}
       >
         <ModalInner tabIndex="0" className="modal-inner" onClick={close}>
-          <div>{selectedCollection.art_uu.art_title}</div>
-          <div>{selectedCollection.art_uu.art_desc}</div>
+          <img
+            src={file_path + selectedCollection.art_uu.art_file_id}
+            alt="img"
+          />
+          <span>
+            <h1>{selectedCollection.art_uu.art_title}</h1>
+            <div>{selectedCollection.art_uu.art_desc}</div>
+          </span>
         </ModalInner>
       </ModalWrapper>
     </>
--- src/App/organisms/Support.js
+++ src/App/organisms/Support.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MainStyle = styled.div`
   display: flex;
+  padding-top: 1em;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
@@ -11,10 +14,12 @@ const ContactStyle = styled.div`
   text-align: center;
   font-family: "Roboto Mono", monospace;
   font-size: 20px;
+  color: #e8eaf6;
+  padding-bottom: 12.5px;
   h1 {
     text-align: center;
     font-family: "Roboto Mono", monospace;
-    font-size: 50px;
+    font-size: 40px;
     font-weight: 800;
   }
   span {
@@ -26,70 +31,122 @@ const ContactStyle = styled.div`
 `;
 const ContactCards = styled.h1`
   display: flex;
+  float: center;
   flex-direction: row;
   text-align: center;
   width: 100%;
   padding: 20px;
+  padding-bottom: 50px;
+  margin: 0.5em;
   div {
+    display: flex;
+    font-family: "Roboto", sans-serif;
+    flex-direction: column;
+    justify-content: space-between;
     box-sizing: content-box;
+    box-shadow: 10px 5px 2px #c08296;
     height: 200px;
+    background-color: #f4b2c7;
     color: black;
     width: 300px;
     border: 5px solid black;
-    margin: 20px;
-    background-color: white;
+    margin-left: 1.2em;
+    margin-right: 1.2em;
     text-align: left;
   }
   text {
     font-size: 15px;
     padding: 20px;
   }
+  p {
+    font-size: 12px;
+    padding: 20px;
+    float: bottom;
+  }
 `;
 
 const Partners = styled.div`
   display: flex;
-  flex-direction: row;
   justify-content: center; /* align horizontal */
   align-items: center; /* align vertical */
-  width: 100%;
-  padding: 20px;
+  max-width: 70%;
+  max-height: 80%;
+  padding-right: 50px;
+  padding-left: 50px;
+  text-align: center;
   a {
+    box-sizing: content-box;
+    height: 150px;
+    width: 300px;
+    margin-top: -30px;
+    margin-right: -50px;
+  }
+  b {
+    box-sizing: content-box;
+    height: 200px;
+    width: 450px;
+    margin-top: -15px;
+    margin-right: -50px;
+  }
+  c {
     box-sizing: content-box;
     height: 200px;
     width: 300px;
-    background-color: hidden;
-    text-align: left;
+    margin-top: 15px;
+  }
+  d {
+    box-sizing: content-box;
+    height: 200px;
+    width: 300px;
+    margin-top: 45px;
+  }
+  e {
+    box-sizing: content-box;
+    height: 200px;
+    width: 300px;
+    margin-top: 25px;
   }
   img {
-    max-width: 100%;
-    height: auto;
+    max-width: 60%;
+    max-height: 60%;
   }
 `;
 const Footer = styled.div`
   display: flex;
   flex-direction: row;
-  place-items: end center;
-  margin: -20px;
-  padding-left: 50px;
-  padding-right: 50px;
+  justify-content: center; /* align horizontal */
+  align-items: center; /* align vertical */
+  width: 150em;
+  margin: -5em;
   background-color: black;
   > :first-child {
+    width: 50em;
+    padding-left: 7.8em;
   }
 `;
 const FootText = styled.div`
-  margin: 110px;
-  padding-right: 150px;
+  margin: 20px;
+  padding-bottom: 2em;
+  color: grey;
   div {
     margin-bottom: 10px;
   }
 `;
 const Socials = styled.div`
-  margin: 130px;
-  padding-left: 150px;
+  display: flex;
+  float: center;
+  margin: 100px;
+  padding-left: 15em;
+  object-fit: cover;
+  img {
+    max-width: 80%;
+    max-height: 80%;
+    margin-left: 0.5em;
+  }
 `;
 
 const Support = () => {
-  //   const file_path = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/file/`;
+  const file_path = `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_API_PORT}/file/`;
   return (
     <MainStyle>
       <ContactStyle>
@@ -97,32 +154,50 @@ const Support = () => {
         <ContactCards>
           <div>
             <text>Are you an artist?</text>
+            <p>
+              <a href="mailto:rheeunion@gmail.com">
+                Department of Publishment
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
           </div>
           <div>
             <text>Are you a private collector?</text>
+            <p>
+              <a href="mailto:nans910521@gmail.com">
+                Department of Curation
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
           </div>
           <div>
             <text>Are you looking for cooperation?</text>
+            <p>
+              <a href="mailto:rnrb555@gmail.com">
+                Department of Business Strategy
+                <FontAwesomeIcon icon={faExternalLinkAlt} />
+              </a>
+            </p>
           </div>
         </ContactCards>
         <span>PARTNERS</span>
       </ContactStyle>
       <Partners>
-        <a href="https://www.bondly.finance/index.html">
-          <img src={file_path + `6`} alt="logo1" />
+        <a href="https://www.kakaocorp.com/">
+          <img src={file_path + `16`} alt="logo1" />
         </a>
-        <a href="https://www.bondly.finance/index.html">
+        <b href="http://www.mmca.go.kr/main.do">
           <img src={file_path + `7`} alt="logo2" />
-        </a>
-        <a href="https://www.bondly.finance/index.html">
+        </b>
+        <c href="https://www.bondly.finance/index.html">
           <img src={file_path + `8`} alt="logo3" />
-        </a>
-        <a href="https://www.bondly.finance/index.html">
-          <img src={file_path + `9`} alt="logo4" />
-        </a>
-        <a href="https://www.bondly.finance/index.html">
-          <img src={file_path + `10`} alt="logo5" />
-        </a>
+        </c>
+        <d href="https://www.navercorp.com/">
+          <img src={file_path + `17`} alt="logo4" />
+        </d>
+        <e href="https://www.bondly.finance/">
+          <img src={file_path + `6`} alt="logo5" />
+        </e>
       </Partners>
       <Footer>
         <FootText>
@@ -134,14 +209,14 @@ const Support = () => {
           Do Not Sell My <span>Personal Information</span>.
         </FootText>
         <Socials>
-          <a href="https://www.bondly.finance/index.html">
-            <img src={file_path + `10`} alt="facebook" />
+          <a href="https://www.facebook.com/romanticjay">
+            <img src={file_path + `13`} alt="facebook" />
           </a>
-          <a href="https://www.bondly.finance/index.html">
-            <img src={file_path + `10`} alt="Youtube" />
+          <a href="https://www.youtube.com/watch?v=n53T1MMDpXY">
+            <img src={file_path + `15`} alt="Youtube" />
           </a>
-          <a href="https://www.bondly.finance/index.html">
-            <img src={file_path + `10`} alt="LinkedIn" />
+          <a href="https://www.linkedin.com/showcase/secretnetwork/">
+            <img src={file_path + `14`} alt="LinkedIn" />
           </a>
         </Socials>
       </Footer>
